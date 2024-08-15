35e769674c9e738c99c2d28076d9dee9f0547690
Gondaki <joao.gondaki@edu.unipar.br>
2024-08-14 22:08:56 -0300

    Atividade 1

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    // Declarando variáveis
+    val id1: Int =18
+    val id2: Int = 30
+    val alturaPessoa1: Double = 1.75
+    val nomePessoa1: String = "João"
+    val eEstudantePessoa1: Boolean = true
+
+    // Calculando a soma das idades
+    val somaIdades: Int = id1 + id2
+
+    // Exibindo os valores
+    println("Nome: $nomePessoa1")
+    println("Idade: $id1")
+    println("Altura: $alturaPessoa1 metros")
+    println("É estudante? $eEstudantePessoa1")
+    println("Soma das idades: $somaIdades")
+}
